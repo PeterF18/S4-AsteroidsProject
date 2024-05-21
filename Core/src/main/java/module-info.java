0a841884af192321ef844f99552fff6708e565f0
Peter Fischer <pefis22@student.sdu.dk>
2024-05-21 20:54:08 +0200

    Module-info cleanup
    Removed a wrong dependency on CommonBullet from Core

--- Core/src/main/java/module-info.java
+++ Core/src/main/java/module-info.java
@@ -4,7 +4,6 @@ import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
 
 module Core {
     requires Common;
-    requires CommonBullet;
     requires javafx.graphics;
     requires spring.context;
     requires spring.core;
