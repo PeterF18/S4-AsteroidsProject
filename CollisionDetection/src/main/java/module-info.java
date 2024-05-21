87c49f6b0773caf3d236ce6fef232f2a44840d6c
Peter Fischer <pefis22@student.sdu.dk>
2024-05-21 21:24:35 +0200

    Renamed Collision module to CollisionDetection
    Refactored all Collision to CollisionDetection following the naming matching the design-idea

--- CollisionDetection/src/main/java/module-info.java
+++ CollisionDetection/src/main/java/module-info.java
@@ -1,6 +1,6 @@
 import dk.sdu.mmmi.cbse.common.services.IPostEntityProcessingService;
 
-module Collision {
+module CollisionDetection {
     requires Common;
     provides IPostEntityProcessingService with dk.sdu.mmmi.cbse.collision.CollisionDetection;
 }
